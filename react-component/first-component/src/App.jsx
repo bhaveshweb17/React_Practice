8b556bb101e47ff5394af25772e38e99f6b83268
bhaveshweb17 <bhaveshwebstudio@gmail.com>
2024-02-07 19:26:45 +0530

    My First React Project Push on github

---
+++ react-component/first-component/src/App.jsx
@@ -0,0 +1,10 @@
+import ConfirmForm from "./confirm"
+import "./App.css"
+function App(){
+  let name = 'Bhavesh Parmar'
+  return <div>
+    <h1>My Name is {name}</h1>
+    <ConfirmForm></ConfirmForm>
+  </div> 
+}
+export default App
